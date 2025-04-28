d8821d006db49362b1f7333929e65331696f46f5
Tag <drago.overlond@gmail.com>
2025-04-28 14:22:09 +0700

    feat(frontend): added shadcn ui & setting auto route in index.jsx

--- week5/frontend/src/main.jsx
+++ week5/frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import routes from "~react-pages";
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+
+const App = () => {
+  return useRoutes(routes)
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
