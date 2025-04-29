f06bdaa44ce2d9b473f5acd33067e88d02aac5dc
Tag <drago.overlond@gmail.com>
2025-04-29 14:32:27 +0700

    feate: create endpoint

--- week5/backend/src/router/index.routes.ts
+++ week5/backend/src/router/index.routes.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 // import { todoRouter } from "./todo.routes.ts";
-import { GetTodo, AddTodo } from "../controller/todo.controller.ts";
+import { GetTodo, AddTodo, EditTodoName, DeleteTodo } from "../controller/todo.controller.ts";
 
 const mainRouter = new Hono();
 
 mainRouter.get("/todo", GetTodo);
 mainRouter.post("/todo", AddTodo);
+mainRouter.put("/todo/:id", EditTodoName);
+mainRouter.delete("/todo/:id", DeleteTodo);
 
 export { mainRouter };
